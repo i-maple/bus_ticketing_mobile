7f42a6bdad1235dd27563d7f20149d3b6980f4ee
Nishant <nishantyt0@gmail.com>
2026-02-14 22:32:06 +0545

    chore(constants): added hive box, app name, and asset path app constants centralization

---
+++ lib/config/app_config.dart
@@ -0,0 +1,9 @@
+abstract final class AppConfig {
+  AppConfig._();
+
+  static const String appName = 'Ticket Booking';
+
+  static const String hiveAppBox = 'app_box';
+
+  static const String seatPlanAssetPath = 'assets/mock/seat_plan.json';
+}
